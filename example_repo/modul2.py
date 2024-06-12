5c9c5b8bf3ad9c7a275d5eae02d9ac5e9985b9f0
kanakraj <t-kanakraj@microsoft.com>
2024-06-13 00:07:27 +0530

    codeparser implemented

---
+++ example_repo/modul2.py
@@ -0,0 +1,5 @@
+from module1 import log
+
+
+def another_function():
+    log("This is a log message from another_function")
---
+++ example_repo/module1.py
@@ -0,0 +1,6 @@
+def log(message):
+    print(message)
+
+
+def some_function():
+    log("This is a log message from some_function")
